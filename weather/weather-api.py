568455c6bcf84ebe8ec0f54eeba00bc06b0bb17d
Nire Bryce <nire@computernope.net>
2025-03-14 20:19:40 +0000

    Mar 14, 2025, 4:19 PM

--- weather/weather-api.py
+++ weather/weather-api.py
@@ -1,5 +1,10 @@
 import requests
 
+import os       # needed for dotenv
+from   dotenv   import load_dotenv
+load_dotenv()
+WEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
 # the first draft of this is me implimenting https://medium.com/@rekalantar/how-to-build-a-simple-weather-app-in-python-with-openweathermap-api-447a2dd27898 the first hit on google just to get a hang of it
 
 API_KEY = "" # TODO: figure out how to have secrets in repo using sops here too, and .env before that
@@ -7,7 +12,7 @@ API_KEY = "" # TODO: figure out how to have secrets in repo using sops here too,
 GPS_LAT     =  42.39142595735906
 GPS_LONG    = -71.12329107896443
 
-URL = f"https://api.openweathermap.org/data/2.5/weather?lat={GPS_LAT}&lon={GPS_LONG}&appid={API_KEY}"
+URL = f"https://api.openweathermap.org/data/2.5/weather?lat={GPS_LAT}&lon={GPS_LONG}&appid={WEATHER_API_KEY}"
 
 def main():
     response = requests.get(URL)
